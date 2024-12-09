b74ae5797412457469891c8d5f01de4bc9ac7b56
Matteo Rigamonti <matteo_rigamonti@hotmail.it>
2024-12-09 18:11:48 +0100

    Modifiche testo cta

--- resources/views/components/sections/cta.blade.php
+++ resources/views/components/sections/cta.blade.php
@@ -1,10 +1,8 @@
 <x-utilities.section>
     <x-utilities.container>
         <div class="text-center">
-            <p class="mb-6 ~sm/lg:~text-2xl/4xl leading-none text-balance">Vuoi saperne di più<br /> <span
-                    class="font-serif font-normal italic">sui
-                    nostri
-                    prodotti.</span></p>
+            <p class="mb-6 ~sm/lg:~text-2xl/4xl leading-none text-pretty">Vuoi saperne di più<br /> <span
+                    class="font-serif font-normal italic">sui nostri prodotti.</span></p>
             <x-utilities.button size="lg">Contattaci</x-utilities.button>
         </div>
     </x-utilities.container>
