c7bbffcaf121e3559975598b22f18ce166b9c387
Matteo Rigamonti <matteo_rigamonti@hotmail.it>
2024-12-09 11:21:51 +0100

    Installati plugin TW modifiche font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,38 @@
+import fluid, { extract, screens } from 'fluid-tailwind';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+    content: {
+        files: [
+            './resources/**/*.blade.php',
+            './resources/**/*.js',
+            './resources/**/*.vue',
+        ],
+        extract
+    },
     theme: {
+        screens,
+        fontSize:{
+            "4xl": '5.25rem',
+            "3xl": ['3.9375rem', {
+                lineHeight: '1'
+            }],
+            "2xl": '3rem',
+            "1xl": ['1.75rem', {
+                lineHeight: '1'
+            }],
+            "xl": '1.5rem',
+            "lg": '1.25rem',
+            "md": '1.125rem',
+            "base": '1rem',
+            "sm": '0.875rem',
+        },
         extend: {
             fontFamily: {
                 sans: ["PPObjectSans", ...defaultTheme.fontFamily.sans],
-                mono: ["'Poppins'", ...defaultTheme.fontFamily.mono]
+                serif: ["Nympha", ...defaultTheme.fontFamily.serif],
+                mono: ["Poppins", ...defaultTheme.fontFamily.mono]
             },
         },
         colors: {
@@ -26,7 +47,8 @@ export default {
             white: '#FFFFFF',
             black: '#000000',
             transparent: 'transparent'
-        }
+        },
     },
-    plugins: [],
+    plugins: [fluid({checkSC144: false}), require('@tailwindcss/container-queries')],
 };
+
