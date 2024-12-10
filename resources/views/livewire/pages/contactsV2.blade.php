33e7ba55ae311b06b73dbf4d9b5a07bc0c61cfb1
Matteo Rigamonti <matteo_rigamonti@hotmail.it>
2024-12-10 11:24:51 +0100

    Aggiunto focus a input

--- resources/views/livewire/pages/contactsV2.blade.php
+++ resources/views/livewire/pages/contactsV2.blade.php
@@ -11,25 +11,25 @@
                 <form class="flex flex-col gap-8 lg:gap-16 items-center w-full">
                     <div class="grid grid-cols-1 lg:grid-cols-2 gap-y-5 gap-x-3">
                         <input
-                            class="px-4 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none"
+                            class="px-7 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none transition-shadow focus:ring ring-orange/25"
                             type="text" placeholder="Nome" />
                         <input
-                            class="px-4 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none"
+                            class="px-7 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none transition-shadow focus:ring ring-orange/25"
                             type="text" placeholder="Cognome" />
                         <input
-                            class="px-4 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none"
+                            class="px-7 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none transition-shadow focus:ring ring-orange/25"
                             type="text" placeholder="Azienda" />
                         <input
-                            class="px-4 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none"
+                            class="px-7 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none transition-shadow focus:ring ring-orange/25"
                             type="phone" placeholder="Cellulare" />
                         <input
-                            class="px-4 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none col-span-full"
+                            class="px-7 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none transition-shadow focus:ring ring-orange/25 col-span-full"
                             type="email" placeholder="Mail" />
                         <input
-                            class="px-4 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none col-span-full"
+                            class="px-7 py-2.5 bg-transparent border rounded-full placeholder:text-black placeholder:font-bold focus:outline-none transition-shadow focus:ring ring-orange/25 col-span-full"
                             type="text" placeholder="Oggetto" />
                         <textarea
-                            class="px-4 py-2.5 bg-transparent resize-none border rounded-3xl placeholder:text-black placeholder:font-bold focus:outline-none col-span-full h-52"
+                            class="px-7 py-2.5 bg-transparent resize-none border rounded-3xl placeholder:text-black placeholder:font-bold focus:outline-none transition-shadow focus:ring ring-orange/25 col-span-full h-52"
                             name="" id="" cols="30" rows="10" placeholder="Scrivi qui la tua richiesta..."></textarea>
                     </div>
 
