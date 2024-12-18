cff16ea0cc0285d47bc59e958937f41bbd8d3313
Luca Pudia <lpudia@2program.it>
2024-12-18 18:24:55 +0100

    Aggiornato il routing per la pagina contatti, sostituendo Contacts con ContactsV2

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', Home::class)->name('home');
 Route::get('/product', Product::class)->name('product');
 Route::get('/about', About::class)->name('about');
-Route::get('/contacts', Contacts::class)->name('contacts');
-Route::get('/components', Components::class)->name('components'); // Da rimuovere
-Route::get('/contactsv2', ContactsV2::class)->name('components'); // Da rimuovere
+Route::get('/contacts', ContactsV2::class)->name('contacts');
