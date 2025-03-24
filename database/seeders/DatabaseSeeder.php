c8600b81135811bdc30749a037da667b0ba59caf
Marco Sansevrino <marco.sansevrino@swypelab.com>
2025-03-24 17:41:53 +0100

    create initial seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Customer;
+use App\Models\Order;
+use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => '2Program Srl',
+            'email' => 'info@2program.it',
+            'password' => bcrypt('admin'),
         ]);
+
+        Product::factory()->count(5)->create();
+
+        Customer::factory()
+            ->count(15)
+            ->has(Order::factory()->count(2), 'orders')
+            ->create();
     }
 }
