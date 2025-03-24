7557541ac68b9e2d9c507c4c0edd6a43f0f1de37
Marco Sansevrino <marco.sansevrino@swypelab.com>
2025-03-24 23:43:16 +0100

    fix factory after added saved data to orders and order rows

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -16,11 +16,35 @@ class OrderFactory extends Factory
 
     public function definition(): array
     {
+        $customer = Customer::all()->random(1)->first();
+
         return [
-            'customer_id' => Customer::all()->random(1)->first()?->id,
+            'customer_id' => $customer?->id,
             'order_number' => $this->faker->unique()->randomNumber(),
             'subtotal' => 0,
             'total' => 0,
+
+            'customer_first_name' => $customer->first_name,
+            'customer_last_name' => $customer->last_name,
+
+            'customer_phone' => $customer->phone,
+
+            'customer_company' => $customer->company,
+            'customer_vat' => $customer->vat,
+            'customer_fiscal_code' => $customer->fiscal_code,
+            'customer_sdi' => $customer->sdi,
+
+            'customer_billing_address' => $customer->billing_address,
+            'customer_billing_city' => $customer->billing_city,
+            'customer_billing_state' => $customer->billing_state,
+            'customer_billing_zip' => $customer->billing_zip,
+            'customer_billing_country' => $customer->billing_country,
+
+            'customer_shipping_address' => $customer->shipping_address,
+            'customer_shipping_city' => $customer->shipping_city,
+            'customer_shipping_state' => $customer->shipping_state,
+            'customer_shipping_zip' => $customer->shipping_zip,
+            'customer_shipping_country' => $customer->shipping_country,
         ];
     }
 
--- database/factories/OrderRowFactory.php
+++ database/factories/OrderRowFactory.php
@@ -24,6 +24,7 @@ class OrderRowFactory extends Factory
         $total = $quantity * $price;
 
         return [
+            'product_title' => $product->title,
             'product_id' => $product->id,
             'quantity' => $quantity,
             'price' => $price,
